9f7ff32409dc03cd78e93d65031092de861365d6
0xLGG <72031534+0xLGG@users.noreply.github.com>
2021-11-15 09:04:16 +0100

    tmp deativated tabnine, added copilot and fugitive

--- lunarvim/config.lua
+++ lunarvim/config.lua
@@ -95,22 +95,23 @@ lvim.builtin.treesitter.highlight.enabled = true
 -- Additional Plugins
 lvim.plugins = {
   {"ChristianChiarulli/vim-solidity"},
-  {
-   "tzachar/cmp-tabnine",
-   config = function ()
-   local tabnine = require('cmp_tabnine.config')
-   tabnine:setup({
-        max_lines = 1000;
-        max_num_results = 20;
-        sort = true;
-    	  run_on_every_keystroke = true;
-	      snippet_placeholder = '..';
-     })
-  end,
+  -- {
+  --  "tzachar/cmp-tabnine",
+  --  config = function ()
+  --  local tabnine = require('cmp_tabnine.config')
+  --  tabnine:setup({
+  --       max_lines = 1000;
+  --       max_num_results = 20;
+  --       sort = true;
+  --   	  run_on_every_keystroke = true;
+	 --      snippet_placeholder = '..';
+  --    })
+  -- end,
 
-   run = "./install.sh",
-   requires = "hrsh7th/nvim-cmp",
-  },
+  --  run = "./install.sh",
+  --  requires = "hrsh7th/nvim-cmp",
+  -- },
+  {"tpope/vim-fugitive"},
   {"https://github.com/github/copilot.vim"}
 
 --     {"folke/tokyonight.nvim"}, {
